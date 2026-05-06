a1873ed1e2292d6603a66e24758101d86705ffc1
xy7917 <xuyue7917@outlook.com>
2026-05-06 23:40:26 +0800

    first commit

---
+++ 2236.判断根结点是否等于子结点之和.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=2236 lang=typescript
+ *
+ * [2236] 判断根结点是否等于子结点之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function checkTree(root: TreeNode | null): boolean {
+    return root!.val === root!.left!.val + root!.right!.val;
+};
+// @lc code=end
+
